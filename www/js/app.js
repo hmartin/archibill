0a3b419ee38882029a5fbbce3bedcdad38febed0
hmartin <hmartin.fr@gmail.com>
2014-01-21 22:48:14 +0100

    lib

--- www/js/app.js
+++ www/js/app.js
@@ -8,8 +8,21 @@ myApp.config(function($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
+angular.module('ngView', [], function($routeProvider, $locationProvider) {
+  $routeProvider.when('/Book/:bookId', {
+    templateUrl: 'book.html',
+    controller: BookCntl
+  });
+  $routeProvider.when('/Book/:bookId/ch/:chapterId', {
+    templateUrl: 'chapter.html',
+    controller: ChapterCntl
+  });
+ 
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
+});
 myApp.config(['$routeProvider', function($routeProvider, $httpProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/ranking.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
