66d0565dadfc668d05b96ad35f79afb277d0c46e
hmartin <hmartin.fr@gmail.com>
2014-01-22 12:54:20 +0100

    ionic

--- www/js/app.js
+++ www/js/app.js
@@ -1,28 +1,69 @@
-'use strict';
+// Ionic Starter App, v0.9.20
 
-var myApp = angular.module('myApp', [
-  'ngRoute',
-  'myApp.controllers'
-]);
-myApp.config(function($httpProvider) {
-    $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+
+    // the pet tab has its own child nav-view and history
+    .state('tab.pet-index', {
+      url: '/pets',
+      views: {
+        'pets-tab': {
+          templateUrl: 'templates/pet-index.html',
+          controller: 'PetIndexCtrl'
+        }
+      }
+    })
+
+    .state('tab.pet-detail', {
+      url: '/pet/:petId',
+      views: {
+        'pets-tab': {
+          templateUrl: 'templates/pet-detail.html',
+          controller: 'PetDetailCtrl'
+        }
+      }
+    })
+
+    .state('tab.adopt', {
+      url: '/adopt',
+      views: {
+        'adopt-tab': {
+          templateUrl: 'templates/adopt.html'
+        }
+      }
+    })
+
+    .state('tab.about', {
+      url: '/about',
+      views: {
+        'about-tab': {
+          templateUrl: 'templates/about.html'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/pets');
+
 });
-angular.module('ngView', [], function($routeProvider, $locationProvider) {
-  $routeProvider.when('/Book/:bookId', {
-    templateUrl: 'book.html',
-    controller: BookCntl
-  });
-  $routeProvider.when('/Book/:bookId/ch/:chapterId', {
-    templateUrl: 'chapter.html',
-    controller: ChapterCntl
-  });
- 
-  // configure html5 to get links working on jsfiddle
-  $locationProvider.html5Mode(true);
-});
-myApp.config(['$routeProvider', function($routeProvider, $httpProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/ranking.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/'});
-}]);
+
