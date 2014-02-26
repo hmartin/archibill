d7d3aeb1dafb795956d22b91636e1b0736afd01e
hmartin via nitrous <hmartin.fr@gmail.com>
2014-02-26 14:48:15 +0000

    photo directive

--- www/js/services.js
+++ www/js/services.js
@@ -19,6 +19,7 @@ angular.module('starter.services', [])
           var db = window.openDatabase("Checkbill", "1.0", "Checkbill Info", 200000);
           values = typeof values !== 'undefined' ? values : null;
           onSuccessFct = typeof onSuccessFct !== 'undefined' ? onSuccessFct : null;
+          console.log('tmp');
           console.log(queries[queryId]);console.log(values);
           console.log(db);
           db.transaction(function (tx) {
